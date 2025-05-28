e6d9390cba7537ad3eb0d948fa184118c420de2e
princewill863 <iroka@sheridancollege.ca>
2025-05-27 20:23:36 -0400

    Completed the CardTrick class so that it models a “hand” of seven cards

--- src/card/Card.java
+++ src/card/Card.java
@@ -2,6 +2,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+//Name: Princewill Iroka
+//Student number: 991788185
+
 package card;
 
 /**
